dac7fc99636d758a3147e96a44da920e706faa43
rick-love <ricklove@outlook.com>
2023-12-05 17:32:20 +0100

    Fixing Page.ts

--- types/Page.ts
+++ types/Page.ts
@@ -1,7 +1,8 @@
+import { ReactNode } from "react"
 import { PortableTextBlock } from "sanity"
 
 export type Page = {
-    title: ReactNode
+    title: ReactNode,
     _id: string,
     _createdAt: Date,
     name: string,
